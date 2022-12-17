3f31026ecf66b8dddac3b6f0b41342c039729705
Pawel Gorniak <p_gorniak@tlen.pl>
2022-12-17 17:29:07 +0100

    Update js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  const newGalleryItem = document.createElement("div");
+  newGalleryItem.classList.add("gallery__item");
+  const newGalleryLink = document.createElement("a");
+  newGalleryLink.classList.add("gallery__link");
+  newGalleryLink.href = item.original;
+  const newGalleryImg = document.createElement("img");
+  newGalleryImg.classList.add("gallery__image");
+  newGalleryImg.src = item.preview;
+  newGalleryImg.dataset.source = item.original;
+  newGalleryImg.alt = item.description;
+  gallery.appendChild(newGalleryItem);
+  newGalleryItem.appendChild(newGalleryLink);
+  newGalleryLink.appendChild(newGalleryImg);
+}
+
+const getOriginalImg = (e) => {
+  e.preventDefault();
+  const selectedImg = e.target.dataset.source;
+  const instance = basicLightbox.create(`<img src=${selectedImg}>`);
+  instance.show();
+
+  const visible = instance.visible();
+  const closeWithEscKey = (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  };
+  if (visible === true) {
+    gallery.addEventListener("keydown", closeWithEscKey);
+  } else {
+    gallery.removeEventListener("keydown", closeWithEscKey);
+  }
+};
+
+gallery.addEventListener("click", getOriginalImg);
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  const newGalleryItem = document.createElement("a");
+  newGalleryItem.classList.add("gallery__item");
+  newGalleryItem.href = item.original;
+  const newGalleryImg = document.createElement("img");
+  newGalleryImg.classList.add("gallery__image");
+  newGalleryImg.src = item.preview;
+  newGalleryImg.alt = item.description;
+  gallery.appendChild(newGalleryItem);
+  newGalleryItem.appendChild(newGalleryImg);
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
